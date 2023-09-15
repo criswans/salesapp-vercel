c019e6742ccf77c0fac55249310c5e5d495504b7
Ernest Kristianto <kristiantoernest@gmail.com>
2023-09-15 20:39:47 +0700

    add get company

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request, make_response
 
 app = Flask(__name__)
 
@@ -9,3 +9,30 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route('/get_company', methods=['GET'])
+def get_company():
+    
+    company_id =  request.args.get("company_id")
+    
+    # try:
+    #     dt = Data()
+    #     values = ()
+    #     if request.method == "GET":
+    #         if company_id:
+    #             query = "SELECT * FROM company where company_id = %s"
+    #             values = (company_id,)
+    #         else:
+    #             query =  "SELECT * FROM company"
+
+    #     data = dt.get_data(query, values)
+             
+
+    # except Exception as e:
+    #     return make_response(jsonify({
+    #         "status" : "FAILED",
+            # "error" : str(e)}), 400)
+    
+    return make_response(jsonify({
+        "status" : "SUCCESS",
+        "data" : company_id}), 200)
