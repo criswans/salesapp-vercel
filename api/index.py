680db578b29753424693eebdafcef43feb278c2a
Ernest Kristianto <kristiantoernest@gmail.com>
2023-09-15 18:48:49 +0700

    merge fi;e

--- api/index.py
+++ api/index.py
@@ -1,5 +1,43 @@
 from flask import Flask, jsonify, request, make_response
-from model import Data
+
+import mysql.connector
+
+def conn(user = "root", password = "", host = "localhost", database = "skripsi"):
+    conn = mysql.connector.connect(
+        host = host,
+        user = user,
+        password = password,
+        database = database
+    )
+    return conn
+
+
+def select(query, values, conn):
+    myCursor = conn.cursor()
+    myCursor.execute(query, values)
+    row_headers = [x[0] for x in myCursor.description]
+    myResult = myCursor.fetchall()
+    json_data = []
+
+    for result in myResult:
+        json_data.append(dict(zip(row_headers, result)))
+    return json_data
+
+
+def insert(query, val, conn):
+    myCursor = conn.cursor()
+    myCursor.execute(query, val)
+    conn.commit()
+
+class Data:
+    def __init__(self):
+        self.mydb = conn()
+
+    def get_data (self, query, values):
+        return select(query, values, self.mydb)
+    
+    def insert_data (self, query, val):
+        return insert(query, val, self.mydb)
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
